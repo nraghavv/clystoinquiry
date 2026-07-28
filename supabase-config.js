d1a1f6f6a2b2ca1bb851118b7632e4113cc065f4
nraghavv <nvraghav2008@gmail.com>
2026-07-28 23:58:04 +0530

    commit database

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-     url: "https://oonjohhbuhspcpwpsmdt.supabase.co",
-     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbmpvaGhidWhzcGNwd3BzbWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDkxMzgsImV4cCI6MjEwMDgyNTEzOH0.GSzP-k6U4xHlN1yGctXu7V-fcd6zD0eTMmjF5MEProc"
-   };
+  url: "https://oonjohhbuhspcpwpsmdt.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbmpvaGhidWhzcGNwd3BzbWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDkxMzgsImV4cCI6MjEwMDgyNTEzOH0.GSzP-k6U4xHlN1yGctXu7V-fcd6zD0eTMmjF5MEProc"
+};
